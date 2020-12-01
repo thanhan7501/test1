f5094819a9e88008f3d2392843afe4dc5427c689
thanhan7501 <anntgch190372@fpt.edu.vn>
2020-12-01 10:42:07 +0700

    update delete

--- server.js
+++ server.js
@@ -33,7 +33,39 @@ app.get('/', (req, res) => {
     res.render('all', { data: userJson })
 });
 
+app.get('/delete', (req, res) => {
+    let NameToDelete = req.query.name;
+    //1. load data from file to array
+    let fileContent = fs.readFileSync(fileName, 'utf-8');
+    let userText = fileContent.split('/');
+    userText.shift();
+    let userJson = [];
+    userText.forEach(user => {
+        let eachUser = {
+            'name': user.split(':')[0],
+            'city': user.split(':')[1]
+        }
+        userJson.push(eachUser);
+    });
 
+    //2. find user in array and delete
+    let indexToRemove = -1;
+    for (i = 0; i < userJson.length; i++) {
+        if (userJson[i].name == NameToDelete) {
+            indexToRemove = i;
+            break;
+        }
+    }
+    userJson.splice(indexToRemove, 1);
+    
+    //3. save array to file
+    let contentToSave = '';
+    for (i = 0; i < userJson.length; i++) {
+        contentToSave += '/' + userJson[i].name + ':' + userJson[i].city;
+    }
+    fs.writeFileSync(fileName, contentToSave);
+    res.redirect('/');
+});
 
 
 var PORT = process.env.Port || 8000
